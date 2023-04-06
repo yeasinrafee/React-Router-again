1ce77422c5cbbd574cf3c277365a5b94bba4ef9a
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-04-06 15:11:56 +0600

    showing friend details to ui

--- src/components/FriendDetail/FriendDetail.jsx
+++ src/components/FriendDetail/FriendDetail.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import "./FriendDetail.css";
 import { useLoaderData } from "react-router-dom";
 
 const FriendDetail = () => {
   const data = useLoaderData();
-  console.log(data);
   return (
     <div>
       <h2>Everything about friend is here</h2>
+      <div className="friend-detail">
+        <h1>Name: {data.name}</h1>
+        <p>User Name: {data.username}</p>
+        <p>Phone No: {data.phone}</p>
+        <p>Website: {data.website}</p>
+      </div>
     </div>
   );
 };
