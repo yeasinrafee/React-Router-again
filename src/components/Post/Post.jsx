dd9d1dc1f5267504cc0ed5610fffad1ab630cfa2
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-04-06 15:29:17 +0600

    Button added with use navigate hook

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import "./Post.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Post = ({ post }) => {
+  const navigate = useNavigate();
+  const detailsHandler = () => {
+    navigate(`/posts/${post.id}`);
+  };
   return (
     <div className="post">
       <h2>Post No: {post.id} </h2>
       <h3>Title: {post.title}</h3>
       <p>{post.body}</p>
       <Link to={`/posts/${post.id}`}>Show more...</Link>
+      <button onClick={detailsHandler}>Details</button>
     </div>
   );
 };
