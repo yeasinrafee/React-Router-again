9cb6b34fb13b8d818a0d983714ef7d13ac464726
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-04-06 15:15:46 +0600

    Header styled

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import "./Header.css";
 import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
+    <div className="header">
       <Link to={`/`}>Home</Link>
       <Link to={`about`}>About</Link>
       <Link to={`contact`}>Contact</Link>
